5431b03737e82615a0299a76ace8e21a8ecb60ea
zhangpeiyu <zhangpeiyu4@jd.com>
2019-12-25 21:11:25 +0800

    日志配置

--- src/main/java/com/zpy/springboot/service/HelloService.java
+++ src/main/java/com/zpy/springboot/service/HelloService.java
@@ -1,4 +1,11 @@
 package com.zpy.springboot.service;
 
+import java.util.concurrent.Callable;
+import java.util.logging.Logger;
+
 public class HelloService {
+    private Logger logger = Logger.getLogger("HelloService");
+
+    Callable<HelloService> callable;
+
 }
--- src/test/java/com/zpy/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/zpy/springboot/SpringbootApplicationTests.java
@@ -1,9 +1,10 @@
 package com.zpy.springboot;
 
 import com.zpy.springboot.domain.Person;
-import com.zpy.springboot.service.HelloService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class SpringbootApplicationTests {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringbootApplicationTests.class);
 
     @Autowired
     private Person person;
@@ -20,9 +22,17 @@ public class SpringbootApplicationTests {
     private ApplicationContext applicationContext;
 
     @Test
-    public void contextLoads() {
+    public void testAnnotation() {
         System.out.println(person);
         System.out.println(applicationContext.containsBean("helloService2"));
     }
 
+    @Test
+    public void testLogLevel() {
+        LOGGER.trace("trace...");
+        LOGGER.debug("debug...");
+        LOGGER.info("info...");
+        LOGGER.warn("warn...");
+        LOGGER.error("error...");
+    }
 }
